876279019f295b4974b3b17ad45f5516bca51e3b
Yaroslava <yaroslava.ugoleva@gmail.com>
2020-06-14 19:40:38 +0200

    Update cacheSolve.R
    Comments added

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,3 +1,6 @@
+#calculates the inverse of a matrix. If it was already been calculated and stored in cache, the if block is being executed, and the previous value retrieved. In other cases, the inverse is being calculated from scratch. 
+
+
 cacheSolve <- function(a, ...) {
         #attach(cacheMatrix)
         inv <-with(cacheMatrix, a$getinverse())
